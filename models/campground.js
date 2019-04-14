bb5875633bcd6e61591a7994458013eec794ea73
Saurabht83 <42310394+Saurabht83@users.noreply.github.com>
2019-04-14 20:37:56 +0530

    Create campground.js

---
+++ models/campground.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+
+var campSchema =new  mongoose.Schema({
+    
+   name:String,
+   image: String,
+    author: {
+            id: {
+               type: mongoose.Schema.Types.ObjectId,
+               ref: "User" 
+                
+            },
+            username:String
+            
+            
+        },
+   description:String,
+   comments:[
+       {
+           type: mongoose.Schema.Types.ObjectId,
+           ref: "Comment"
+           
+       }
+       ]
+    
+       });
+
+module.exports = mongoose.model("Campground", campSchema);
